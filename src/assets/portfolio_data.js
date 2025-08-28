7e28579fe9880b71af0e8978c064e708448532c2
Jenni Mikkonen <Jenni.Mikkonen3@edu.savonia.fi>
2025-08-28 15:48:44 +0300

    Add Time Tracking Dashboard to portfolio

--- src/assets/portfolio_data.js
+++ src/assets/portfolio_data.js
@@ -1,21 +1,27 @@
 import portfolio_site_img from '../assets/portfolio_site_img.png';
+import time_tracking_db_img from '../assets/time_tracking_db_img.jpg';
 import datafetch_app_img from '../assets/datafetch_app_img.png';
 import task_manager_img from '../assets/task_manager_img.png';
-// import csv_cleaner_img from '../assets/csv_cleaner_img.png';
 // import ecommerce_page_img from '../assets/ecommerce_page_img.png';
 // import rps_game_img from '../assets/rps_game_img.png';
+// import csv_cleaner_img from '../assets/csv_cleaner_img.png';
 import placeholder_img from '../assets/placeholder_img.png';
 
 const portfolio_data = [
     {
-        title: "DataFetch App",
-        w_img: datafetch_app_img,
-        link: "https://github.com/jebbujebbu/datafetch_app"
-    },
-    {
         title: "Portfolio Website",
         w_img: portfolio_site_img,
         link: "https://github.com/jebbujebbu/portfolio_site"
+    },
+    {
+        title: "Time Tracking Dashboard",
+        w_img: time_tracking_db_img,
+        link: "https://github.com/jebbujebbu/time-tracking-dashboard"
+    },
+    {
+        title: "DataFetch App",
+        w_img: datafetch_app_img,
+        link: "https://github.com/jebbujebbu/datafetch_app"
     },
     {
         title: "Task Manager",
